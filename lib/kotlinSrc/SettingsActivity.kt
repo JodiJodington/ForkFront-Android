aae0d97a477a6e5d1ee5049cc628d66252790cef
JodiJodington <193165323+JodiJodington@users.noreply.github.com>
2026-06-10 09:46:22 -0500

    addede compose deps and a dummy settings activity

---
+++ lib/kotlinSrc/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package com.tbd.forkfront
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+public class SettingsActivity : ComponentActivity() {
+    override fun onCreate(savedInsance: Bundle?) {
+        super.onCreate(savedInsance)
+        setContent {
+            Text("Hello, World!")
+        }
+    }
+}
