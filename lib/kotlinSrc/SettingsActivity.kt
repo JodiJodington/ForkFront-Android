432c33bac2c8ddcb8409ad46c220b44bd7a605c7
JodiJodington <193165323+JodiJodington@users.noreply.github.com>
2026-06-11 10:32:50 -0500

    trying a thing

--- lib/kotlinSrc/SettingsActivity.kt
+++ lib/kotlinSrc/SettingsActivity.kt
@@ -3,24 +3,60 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.background
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.State
 
 
 public class SettingsActivity : ComponentActivity() {
-    override fun onCreate(savedInsance: Bundle?) {
-        super.onCreate(savedInsance)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContent {
-            Text(
-                "Hello, World!",
-                modifier = Modifier.fillMaxSize().background(Color.Red),
-                color = Color.Blue
-            )
+            Box(
+                modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.background)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxHeight().background(color = MaterialTheme.colorScheme.surface).align(Alignment.Center)
+                ) {
+                    SettingsSwitch(title = "Fullscreen")
+                }
+            }
         }
     }
 }
+
+@Composable
+fun SettingsSwitch(title: String) {
+    var checked = remember { mutableStateOf(true) }
+    Box(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            title,
+            style = MaterialTheme.typography.labelLarge,
+            modifier = Modifier.align(Alignment.CenterStart)
+        )
+        Switch(
+            checked = checked.value,
+            onCheckedChange = { bool: Boolean ->
+                checked.value = bool
+            },
+            modifier = Modifier.align(Alignment.CenterEnd)
+        )
+    }
+}
+
